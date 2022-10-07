032e8cd5951978dbac45c2615bb2b3002854ee7f
Tri Le <letrin93@gmail.com>
2022-10-07 15:42:11 -0600

    done

--- DaTower.client/src/components/Banner.vue
+++ DaTower.client/src/components/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="Banner py-5 my-3 border border-primary border-3 "
+  <div class="Banner py-5 my-3 border border-primary border-3 " title="banner"
     style="background-image: url(https://images.unsplash.com/photo-1519750157634-b6d493a0f77c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80) ; background-size: cover;, background-position: center;">
     <div class="text-white mx-5 fs-4">
       <p>Get ahead of the scalpers.</p>
